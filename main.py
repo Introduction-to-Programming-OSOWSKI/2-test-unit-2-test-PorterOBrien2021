d2481756812b31444f19526eded0795cd9167a30
PorterOBrien2021 <824085@mystma.org>
2021-01-15 15:47:53 +0000

    test

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#WRITE YOUR CODE IN THIS FILE
+def waterState(f):
+    if f == "Ice":
+        return "Solid"
+    elif f == "water":
+        return "Liquid"
+    else:
+        return "gas"
+def isDozen (d):
+    if d % 12 == 0:
+        return True
+    else:
+        return False
+def toGerman(a):
+    if a == "yes":
+        return "ja"
+    else:
+        return "nein"
+def stopLight(c):
+    if c == "green":
+        return "go"
+    elif c == "yellow":
+        return "slow"
+    else:
+        return "stop"
+
